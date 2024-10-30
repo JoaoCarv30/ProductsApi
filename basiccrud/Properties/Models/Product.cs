4f58f55f303875a477f98da9848a488d99fb3dc6
Joao Victor <Carvalho.joao1@gmail.com>
2024-10-30 17:24:06 -0300

    Model Criada

--- basiccrud/Properties/Models/Product.cs
+++ basiccrud/Properties/Models/Product.cs
@@ -1,8 +1,22 @@
-﻿namespace basiccrud.Properties.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace basiccrud.Properties.Models;
 
 public class Product
 {
+    [Key]
+    public int Id { get; set; }
     
+    [Required]
+    public string Name { get; set; }
     
+    [Required]
+    public double Price { get; set; }
+    
+    [Required]
+    public string Image { get; set; }
+    
+    [Required]
+    public int Quantity { get; set; }
     
 }
